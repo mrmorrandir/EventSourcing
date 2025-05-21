ffc6397e7dfab54beeea8f846ea41c2a2986597a
Andreas Naumann <43349286+mrmorrandir@users.noreply.github.com>
2025-05-21 08:11:06 +0200

    Added Benchmarks

---
+++ tests/EventSourcing.SourceGenerators.Benchmarks/Program.cs
@@ -0,0 +1,9 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Running;
+using EventSourcing.SourceGenerators.Benchmarks;
+
+var config = ManualConfig
+    .CreateMinimumViable()
+    .WithOptions(ConfigOptions.DisableOptimizationsValidator);
+
+BenchmarkRunner.Run<EventRegistryBenchmarks>(config);
